d607f48d806ea7a940f298ec5e38eea8cc3ecc29
Jose Simancas <josesimancas51@gmail.com>
2025-01-08 23:41:47 -0500

    Ignoring ts error on app building

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // @ts-expect-error, there are weird type issues with defineConfig and test property.
   test: {
     globals: true,
     environment: "jsdom",
